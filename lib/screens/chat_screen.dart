0462708fb074025054d5217a2a9c15beaca4acfd
diallo_sorydi3 <ibrahimasory22@hotmail.com>
2020-07-15 19:22:26 +0200

    done

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -14,6 +14,103 @@ class _ChatScreenState extends State<ChatScreen> {
   final User user;
   _ChatScreenState({this.user});
 
+  dynamic _buildMessage(Message message, bool isMe) {
+    final Container msg = Container(
+      decoration: isMe
+          ? BoxDecoration(
+              color: isMe ? Theme.of(context).accentColor : Color(0xFFFFEFEE),
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(15.0),
+                  bottomLeft: Radius.circular(15.0)),
+            )
+          : BoxDecoration(
+              color: isMe ? Theme.of(context).accentColor : Color(0xFFFFEFEE),
+              borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(15.0),
+                  bottomRight: Radius.circular(15.0)),
+            ),
+      margin: isMe
+          ? EdgeInsets.only(
+              top: 7.0,
+              bottom: 8.0,
+              left: 50.0,
+            )
+          : EdgeInsets.only(
+              top: 7.0,
+              bottom: 8.0,
+            ),
+      padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            message.time,
+            style: TextStyle(color: Colors.blueGrey, fontSize: 15.0),
+          ),
+          SizedBox(height: 8.0),
+          Text(message.text,
+              style: TextStyle(
+                color: Colors.blueGrey,
+                fontSize: 15.0,
+              ))
+        ],
+      ),
+    );
+    if (isMe) {
+      return msg;
+    }
+    return Row(
+      children: <Widget>[
+        msg, // return the button and the icon button when is not us the sender of the message
+        IconButton(
+          icon: message.isLiked
+              ? Icon(Icons.favorite)
+              : Icon(Icons.favorite_border),
+          iconSize: 30.0,
+          color: message.isLiked
+              ? Theme.of(context).primaryColor
+              : Colors.blueGrey,
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+
+  _buildMessageComposer() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 8.0),
+      height: 70.0,
+      color: Colors.white,
+      child: Row(
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.photo),
+            iconSize: 20.0,
+            color: Theme.of(context).primaryColor,
+            onPressed: () {},
+          ),
+          Expanded(
+            child: TextField(
+              onChanged: (message) {
+                //retrave inputed data here
+              },
+              textCapitalization: TextCapitalization.sentences,
+              decoration: InputDecoration.collapsed(
+                hintText: 'Send a message ... ',
+              ),
+            ),
+          ),
+          IconButton(
+            icon: Icon(Icons.send),
+            iconSize: 20.0,
+            color: Theme.of(context).primaryColor,
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,29 +136,38 @@ class _ChatScreenState extends State<ChatScreen> {
           ),
         ],
       ),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.green,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30.0),
-                    topRight: Radius.circular(30.0)),
-              ),
-              child: Column(
-                children: <Widget>[
-                  ListView.builder(
+      body: GestureDetector(
+        onDoubleTap: () => FocusScope.of(context).unfocus(), // hide the keyboard when you tab the screen
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30.0),
+                      topRight: Radius.circular(30.0)),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30.0),
+                      topRight: Radius.circular(30.0)),
+                  child: ListView.builder(
+                    reverse: true,
+                    padding: EdgeInsets.only(top: 15),
                     itemCount: messages.length,
                     itemBuilder: (BuildContext context, int idx) {
-                      return Text(messages[idx].text);
+                      Message message = messages[idx];
+                      final bool isMe = message.sender.id == currentUser.id;
+                      return _buildMessage(message, isMe);
                     },
                   ),
-                ],
+                ),
               ),
             ),
-          ),
-        ],
+            _buildMessageComposer(),
+          ],
+        ),
       ),
     );
   }
